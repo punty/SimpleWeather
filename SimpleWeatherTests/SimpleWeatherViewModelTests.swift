94388e1981e4351f9b55e9f17630ad7fddd54a17
Francesco Puntillo <francesco.puntillo@gmail.com>
2017-12-12 15:34:21 +0000

    Fix Unit tests

--- SimpleWeatherTests/SimpleWeatherViewModelTests.swift
+++ SimpleWeatherTests/SimpleWeatherViewModelTests.swift
@@ -19,55 +19,54 @@ class SimpleWeatherViewModelTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        itemsArray.removeAll()
-        cityArray.removeAll()
-        enableArray.removeAll()
-        errorsArray.removeAll()
-        enableRowsArray.removeAll()
+        itemsArray = [[]]
+        cityArray = []
+        enableArray = []
+        errorsArray = []
+        enableRowsArray = []
     }
     
-    func prepareBindings() -> WeatherViewModel.Bindings {
-        
-        let itemsExpect = expectation(description: "items")
-        let update: ([SectionModel<String,WeatherCellViewModel>]) -> Void = { [weak self] (items: [SectionModel<String,WeatherCellViewModel>]) in
-            self?.itemsArray.append(items)
-            itemsExpect.fulfill()
-        }
-        let updateCity: (String) -> Void = { [weak self] (cityInfo: String) in
-            self?.cityArray.append(cityInfo)
-        }
-        let enableSearch: (Bool) -> Void = { [weak self] (enable: Bool) in
-           self?.enableArray.append(enable)
-        }
-        let showError: (String) -> Void   = { [weak self] (error: String) in
-            self?.errorsArray.append(error)
-        }
-        let enableRows: (Int) -> Void = { [weak self] (rows: Int) in
-            self?.enableRowsArray.append(rows)
-        }
-        return WeatherViewModel.Bindings(updateCollectionView: update, showError: showError, enableSearch: enableSearch, enableRows: enableRows, updateCityInfo: updateCity)
+    private func prepareBindings(setupExpectation: Bool = false) ->
+        WeatherViewModel.Bindings {
+            var itemsExpect: XCTestExpectation?
+            if setupExpectation {
+                itemsExpect = expectation(description: "items")
+            }
+            
+            let update: ([SectionModel<String,WeatherCellViewModel>]) -> Void = { [weak self] (items: [SectionModel<String,WeatherCellViewModel>]) in
+                self?.itemsArray.append(items)
+                itemsExpect?.fulfill()
+            }
+            let updateCity: (String) -> Void = { [weak self] (cityInfo: String) in
+                self?.cityArray.append(cityInfo)
+            }
+            let enableSearch: (Bool) -> Void = { [weak self] (enable: Bool) in
+                self?.enableArray.append(enable)
+            }
+            let showError: (String) -> Void   = { [weak self] (error: String) in
+                self?.errorsArray.append(error)
+            }
+            let enableRows: (Int) -> Void = { [weak self] (rows: Int) in
+                self?.enableRowsArray.append(rows)
+            }
+            return WeatherViewModel.Bindings(updateCollectionView: update, showError: showError, enableSearch: enableSearch, enableRows: enableRows, updateCityInfo: updateCity)
     }
     
-    //when start I should show 0 rows and no city information
-    func testStartingViewModel() {
-        let dependencies = WeatherViewModel.Dependencies(serviceClient: StubNetworkingService())
-        _ = WeatherViewModel(dependencies: dependencies, bindings: prepareBindings())
-        XCTAssertTrue(enableRowsArray[0] == 0)
-        XCTAssertTrue(cityArray[0] == "")
-    }
+    
     
     func testLoadingWhenPressed() {
         let dependencies = WeatherViewModel.Dependencies(serviceClient: StubNetworkingService())
-        let vm = WeatherViewModel(dependencies: dependencies, bindings: prepareBindings())
+        let vm = WeatherViewModel(dependencies: dependencies, bindings: prepareBindings(setupExpectation: true))
         vm.updateText(text: "London")
         vm.buttonPressed()
         waitForExpectations(timeout: 5, handler: nil)
-        XCTAssertTrue(itemsArray[0].count == 5)
+        XCTAssertTrue(itemsArray[0].count == 0)
+        XCTAssertTrue(itemsArray[1].count == 5)
     }
     
     func testDisableSearchButton() {
         let dependencies = WeatherViewModel.Dependencies(serviceClient: StubNetworkingService())
-        let vm = WeatherViewModel(dependencies: dependencies, bindings: prepareBindings())
+        let vm = WeatherViewModel(dependencies: dependencies, bindings: prepareBindings(setupExpectation: true))
         vm.updateText(text: "London")
         vm.buttonPressed()
         waitForExpectations(timeout: 5, handler: nil)
@@ -76,4 +75,14 @@ class SimpleWeatherViewModelTests: XCTestCase {
         XCTAssert(enableArray[1] == true)
     }
     
+    //when start I should show 0 rows and no city information
+    func testStartingViewModel() {
+        let dependencies = WeatherViewModel.Dependencies(serviceClient: StubNetworkingService())
+        _ = WeatherViewModel(dependencies: dependencies, bindings: prepareBindings())
+        XCTAssertTrue(enableRowsArray[0] == 0)
+        XCTAssertTrue(cityArray[0] == "")
+        XCTAssertTrue(itemsArray[0].count == 0)
+    }
+    
 }
+
